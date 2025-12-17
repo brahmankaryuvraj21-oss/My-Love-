c1dabd279e163b2eb0b6347b631f9e1dd4a54734
brahmankaryuvraj21-oss <brahmankaryuvraj21@gmail.com>
2025-12-17 12:42:12 +0530

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
 self.addEventListener("install",e=>{
  e.waitUntil(
-  caches.open("love-cache").then(cache=>{
-   return cache.addAll(["./","./index.html","./style.css","./script.js"]);
-  })
+  caches.open("love-cache").then(c=>c.addAll(["./"]))
  );
 });
 self.addEventListener("fetch",e=>{
- e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request))
- );
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
